0e49083b65a5129597d7fbe55fb03bd25907785f
Jason Yamada-Hanff <jyamada-hanff@chromologic.com>
2019-07-18 02:09:21 -0700

    add BSD3 license to deprecation decorator
    In preparation to include this code in another code base, permissively
    license the code under BSD3.

--- deprecate_defaults.py
+++ deprecate_defaults.py
@@ -1,3 +1,36 @@
+"""Decorator for deprecating default parameter values
+
+License (BSD3):
+
+ Copyright (c) 2019, Jason Yamada-Hanff
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+
+     * Redistributions of source code must retain the above copyright notice,
+       this list of conditions and the following disclaimer.
+     * Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+     * Neither the name of src nor the names of its contributors
+       may be used to endorse or promote products derived from this software
+       without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 import functools
 import inspect
 import warnings
@@ -59,7 +92,7 @@ def deprecated_defaults(*paramnames):
 
     def decorate(func):
         @functools.wraps(func)
-        def deprecated(*args, **kwargs):
+        def wrapped(*args, **kwargs):
             unprovided = default_parameters(func) - kwargs.keys()
             deprecations = unprovided  # default to all defaults being required
             if paramnames:  # filter by provided names
@@ -72,9 +105,8 @@ def deprecated_defaults(*paramnames):
                 )
                 warnings.warn(msg, DeprecationWarning, stacklevel=2)
 
-            result = func(*args, **kwargs)
-            return result
+            return func(*args, **kwargs)
 
-        return deprecated
+        return wrapped
 
     return decorate
